ab59254308242165aed40ad1af9d91c06a81ba89
EgaTuts <egatuts@gmail.com>
2014-09-11 14:00:42 +0200

    Config ruby files.

---
+++ public/client/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "sass"
+css_dir = "css"
+images_dir = "img"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = true
+preferred_syntax = :sass
+output_style = :expanded
+relative_assets = true
+Encoding.default_external = Encoding::UTF_8
