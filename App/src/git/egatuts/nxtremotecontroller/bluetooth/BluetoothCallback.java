f65da6fce24b1011a1f371ccf78b4cb58e20c0fa
EgaTuts <egatuts@gmail.com>
2014-11-30 02:26:39 +0100

    Interfaces are now static and changed casing.

--- App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothCallback.java
+++ App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothCallback.java
@@ -1,14 +1,18 @@
 package git.egatuts.nxtremotecontroller.bluetooth;
 
+import git.egatuts.nxtremotecontroller.device.PairedDevice;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+
 public class BluetoothCallback
 {
   
-  public interface onDiscoveryListener
+  public static interface OnDiscoveryListener
   {
-    public void onDiscover ();
+    public void onDiscover (PairedDevice paired_device, BluetoothDevice bluetooth_device, Intent intent);
   }
   
-  public interface onConnectionListener
+  public static interface OnConnectionListener
   {
     public void onConnect ();
     public void onDisconnect();
