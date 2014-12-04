3e50900ef78e46154daea46301aa5b570e45d47a
EgaTuts <egatuts@gmail.com>
2014-12-04 19:01:28 +0100

    Reformatted bluetooth classes.

--- App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothReceiver.java
+++ App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothReceiver.java
@@ -1,3 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2014 EgaTuts & Esaú García - All Rights Reserved                *
+ *                                                                               *
+ * Open-source code licensed under the MIT License (the "License").              *
+ *                                                                               *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  *
+ * of this software and associated documentation files (the "Software"), to deal *
+ * in the Software without restriction, including without limitation the rights  *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+ * copies of the Software, and to permit persons to whom the Software is         *
+ * furnished to do so, subject to the following conditions:                      *
+ *                                                                               *
+ * The above copyright notice and this permission notice shall be included in    *
+ * all copies or substantial portions of the Software.                           *
+ *                                                                               *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
+ * THE SOFTWARE.                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * You can find the entire project at:                                                                                                     *
+ *                                                                                                                                         *
+ *   https://github.com/Egatuts/nxt-remote-controller                                                                                      *
+ *                                                                                                                                         *
+ * And the corresponding file at:                                                                                                          *
+ *                                                                                                                                         *
+ *   https://github.com/Egatuts/nxt-remote-controller/blob/master/App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothReceiver.java *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package git.egatuts.nxtremotecontroller.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
@@ -8,13 +41,35 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import git.egatuts.nxtremotecontroller.bluetooth.BluetoothCallback.OnDiscoveryListener;
 
-public class BluetoothReceiver extends BroadcastReceiver
-{
-  
+public class BluetoothReceiver extends BroadcastReceiver {
+
   private BluetoothCallback.OnDiscoveryListener onDiscoveryListener;
-  
-  public void onReceive (Context context, Intent intent)
-  {
+
+  /*
+   * Returns intent filter to listen for new devices and discovery ending.
+   */
+  public static IntentFilter getIntentFilter () {
+    IntentFilter filter = new IntentFilter();
+    filter.addAction(BluetoothDevice.ACTION_FOUND);
+    filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+    return filter;
+  }
+
+  /*
+   * Getter and setter for OnDiscoveryListener.
+   */
+  public void setOnDiscoveryListener (OnDiscoveryListener listener) {
+    onDiscoveryListener = listener;
+  }
+
+  public OnDiscoveryListener getOnDiscoveryListener () {
+    return onDiscoveryListener;
+  }
+
+  /*
+   * Fired when one of the filtered intent gets fired by the Android system.
+   */
+  public void onReceive (Context context, Intent intent) {
     String action = intent.getAction();
     if (BluetoothDevice.ACTION_FOUND.equals(action)) {
       BluetoothDevice bluetooth_device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
@@ -23,23 +78,5 @@ public class BluetoothReceiver extends BroadcastReceiver
       onDiscoveryListener.onFinish();
     }
   }
-  
-  public void setOnDiscoveryListener (OnDiscoveryListener listener)
-  {
-    onDiscoveryListener = listener;
-  }
-  
-  public OnDiscoveryListener getOnDiscoveryListener ()
-  {
-    return onDiscoveryListener;
-  }
-  
-  public static IntentFilter getIntentFilter ()
-  {
-    IntentFilter filter = new IntentFilter();
-    filter.addAction(BluetoothDevice.ACTION_FOUND);
-    filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-    return filter;
-  }
-  
+
 }
--- App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothUtils.java
+++ App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothUtils.java
@@ -1,3 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2014 EgaTuts & Esaú García - All Rights Reserved                *
+ *                                                                               *
+ * Open-source code licensed under the MIT License (the "License").              *
+ *                                                                               *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  *
+ * of this software and associated documentation files (the "Software"), to deal *
+ * in the Software without restriction, including without limitation the rights  *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+ * copies of the Software, and to permit persons to whom the Software is         *
+ * furnished to do so, subject to the following conditions:                      *
+ *                                                                               *
+ * The above copyright notice and this permission notice shall be included in    *
+ * all copies or substantial portions of the Software.                           *
+ *                                                                               *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
+ * THE SOFTWARE.                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * You can find the entire project at:                                                                                                   *
+ *                                                                                                                                       *
+ *   https://github.com/Egatuts/nxt-remote-controller                                                                                    *
+ *                                                                                                                                       *
+ * And the corresponding file at:                                                                                                        *
+ *                                                                                                                                       *
+ *   https://github.com/Egatuts/nxt-remote-controller/blob/master/App/src/git/egatuts/nxtremotecontroller/bluetooth/BluetoothUtils.java  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package git.egatuts.nxtremotecontroller.bluetooth;
 
 import git.egatuts.nxtremotecontroller.device.PairedDevice;
@@ -8,68 +41,74 @@ import java.util.Set;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
-public class BluetoothUtils
-{
-  
+public class BluetoothUtils {
+
   private BluetoothAdapter bluetooth_adapter;
-  
-  private void initAdapter ()
-  {
+
+  /*
+   * Constructor.
+   */
+  private void initAdapter () {
     bluetooth_adapter = BluetoothAdapter.getDefaultAdapter();
   }
-  
-  public BluetoothUtils ()
-  {
+
+  public BluetoothUtils () {
     initAdapter();
   }
-  
-  public BluetoothAdapter getAdapter ()
-  {
+
+  /*
+   * Returns the adapter if exists or creates a new instance.
+   */
+  public BluetoothAdapter getAdapter () {
     if (bluetooth_adapter == null) initAdapter();
     return bluetooth_adapter;
   }
-  
-  public boolean isEnabled ()
-  {
+
+  /*
+   * State checkers for enabled and discovering.
+   */
+  public boolean isEnabled () {
     return getAdapter().isEnabled();
   }
-  
-  public boolean isDiscovering ()
-  {
+
+  public boolean isDiscovering () {
     return getAdapter().isDiscovering();
   }
-  
-  public boolean startDiscovery ()
-  {
+
+  /*
+   * Starts and cancels bluetooth discovery.
+   */
+  public boolean startDiscovery () {
     return this.getAdapter().startDiscovery();
   }
-  
-  public boolean cancelDiscovery ()
-  {
+
+  public boolean cancelDiscovery () {
     return this.getAdapter().startDiscovery();
   }
-  
-  public boolean enable ()
-  {
+
+  /*
+   * Enables and disables bluetooth adapter.
+   */
+  public boolean enable () {
     return this.getAdapter().enable();
   }
-  
-  public boolean disable ()
-  {
+
+  public boolean disable () {
     return this.getAdapter().disable();
   }
-  
-  public ArrayList<PairedDevice> getDevices ()
-  {
+
+  /*
+   * Returns all bonded devices as an ArrayList of PairedDevice instances.
+   */
+  public ArrayList<PairedDevice> getDevices () {
     Set<BluetoothDevice> devices = bluetooth_adapter.getBondedDevices();
     ArrayList<PairedDevice> devices_set = new ArrayList<PairedDevice>();
     PairedDevice new_device;
-    for (BluetoothDevice old_device : devices)
-    {
+    for (BluetoothDevice old_device: devices) {
       new_device = PairedDevice.from(old_device);
       devices_set.add(new_device);
     }
     return devices_set;
   }
-  
+
 }
