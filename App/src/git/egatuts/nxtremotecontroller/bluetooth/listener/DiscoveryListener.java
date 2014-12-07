0f5600c541ec11f146c63bfd2d8fb448fa800086
EgaTuts <egatuts@gmail.com>
2014-12-07 22:52:48 +0100

    Fixed indentation.

--- App/src/git/egatuts/nxtremotecontroller/bluetooth/listener/DiscoveryListener.java
+++ App/src/git/egatuts/nxtremotecontroller/bluetooth/listener/DiscoveryListener.java
@@ -5,18 +5,18 @@ import android.content.Intent;
 
 public abstract class DiscoveryListener extends BaseListener {
 
-  @Override public void onConnectionChange (Context context, Intent intent) {}
-  //@Override public void onDiscoveryFinish  (Context context, Intent intent) {}
-  //@Override public void onDiscoveryStart   (Context context, Intent intent) {}
-  @Override public void onLocalNameChange  (Context context, Intent intent) {}
-  @Override public void onScanModeChange   (Context context, Intent intent) {}
-  @Override public void onStateChange      (Context context, Intent intent) {}
+  @Override public void onConnectionChange  (Context context, Intent intent) {}
+  //@Override public void onDiscoveryFinish (Context context, Intent intent) {}
+  //@Override public void onDiscoveryStart  (Context context, Intent intent) {}
+  @Override public void onLocalNameChange   (Context context, Intent intent) {}
+  @Override public void onScanModeChange    (Context context, Intent intent) {}
+  @Override public void onStateChange       (Context context, Intent intent) {}
   @Override public void onLowLevelConnect           (Context context, Intent intent) {}
   @Override public void onLowLevelDisconnect        (Context context, Intent intent) {}
   @Override public void onLowLevelDisconnectRequest (Context context, Intent intent) {}
   @Override public void onBondStateChange           (Context context, Intent intent) {}
   @Override public void onDeviceClassChange         (Context context, Intent intent) {}
-  //@Override public void onDeviceFound               (Context context, Intent intent) {}
+  //@Override public void onDeviceFound             (Context context, Intent intent) {}
   @Override public void onRemoteNameChange          (Context context, Intent intent) {}
   
 }
